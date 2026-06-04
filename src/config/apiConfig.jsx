1058f91191c1aaea3952002bb51cb81318d60dcd
vijeta <vijetavarma1@gmail.com>
2026-06-04 21:26:42 +0530

    config api changed

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL:
-    "https://e-comm-backend-nwg4icv3u-vijeta-varmas-projects.vercel.app/api",
+  baseURL: "https://e-comm-backend-ten-mauve.vercel.app/api",
 });
 
 // 🔐 Attach token automatically
